f91be99d3258dc437d39edfe9062aebb3aaac4d1
webvertize-ro <contact@webvertize.ro>
2026-05-31 19:39:08 +0300

    further changes

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://izbziivdfptdnnnkjcxs.supabase.co";
+const supabaseUrl = "https://slmicagwqcjumyaipgjj.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6YnppaXZkZnB0ZG5ubmtqY3hzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxOTM4MDYsImV4cCI6MjA3NDc2OTgwNn0.Do1BsRpEpIBzbgADGqvmfeDBsSDyrNo9U_4k9J-MI2A";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsbWljYWd3cWNqdW15YWlwZ2pqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDIzMzc1MCwiZXhwIjoyMDk1ODA5NzUwfQ.ONdrEBTOxNNRzFyAf8x5WoNQ2g8CMCGz3WHZchKyOHE";
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
